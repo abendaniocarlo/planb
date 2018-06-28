70b1f25b3f581d43bc8cbf882f5ea34c78257487
geisher09 <geisherbernabe@gmail.com>
2018-06-28 16:15:13 +0800

    2nd commit

---
+++ application/controllers/planb.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class planb extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('include/header');
+	}
+}
---
+++ application/views/include/header.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+</head>
+	
+<body>
+	<div class="container-fluid">
+	<div id="wrapper">
+		<div id="header">
+			<img src="<?php echo base_url('assets/imgs/bg.jpg');?>">
+		</div>
+		<div id="content">
+			<div id="navbar">
+				<div id="logo"> PLAN B </div>
+				<ul>
+					<li><a href=""> HOME </a></li>
+					<li><a href=""> ABOUT <a></li>
+					<li><a href=""> SERVICES</a></li>
+					<li><a href=""> PRODUCTS </a></li>
+					<li><a href=""> CLIENT </a></li>
+					<li><a href=""> PARTNERSHIP </a></li>
+					<li><a href=""> CONTACT </a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
